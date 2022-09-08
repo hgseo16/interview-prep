9ec3d09d88c697091705271b16346abb753551df
hgseo16 <seohyounggyoo@gmail.com>
2022-09-08 22:01:29 +0900

    threeNumberSums.py

--- easy/array/threeNumberSums.py
+++ easy/array/threeNumberSums.py
@@ -1,3 +1,18 @@
 def threeNumberSum(array, targetSum):
-    # Write your code here.
-    return 1
+    array.sort()
+    triplets = []
+
+    for i in range(len(array)):
+        left = i + 1
+        right = len(array) - 1
+        while (left < right):
+            currentSum = array[i] + array[left] + array[right]
+            if currentSum == targetSum:
+                triplets.append([array[i], array[left], array[right]])
+                left +=1
+                right -=1
+            elif currentSum < targetSum:
+                left += 1
+            elif currentSum > targetSum:
+                right -= 1
+    return triplets
