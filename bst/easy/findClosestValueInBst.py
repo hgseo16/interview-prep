668c350d02e01587665770a406f3b68ade4e64b5
hgseo16 <seohyounggyoo@gmail.com>
2022-09-12 14:57:13 -0400

    findClosestValueInBst.py

---
+++ bst/easy/findClosestValueInBst.py
@@ -0,0 +1,42 @@
+def findClosestValueInBst(tree, target):
+    currValue = tree.value
+    if currValue == target:
+        return currValue
+    elif target < currValue:
+        if tree.left == None:
+            return currValue
+        else:
+            findClosestValueInBst(tree.left, target)
+    elif target <= currValue:
+        if tree.right == None:
+            return currValue
+        else:
+            findClosestValueInBst(tree.right, target)
+
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+data= {
+  "target": 12,
+  "tree": {
+    "nodes": [
+      {"id": "10", "left": "5", "right": "15", "value": 10},
+      {"id": "15", "left": "13", "right": "22", "value": 15},
+      {"id": "22", "left": null, "right": null, "value": 22},
+      {"id": "13", "left": null, "right": "14", "value": 13},
+      {"id": "14", "left": null, "right": null, "value": 14},
+      {"id": "5", "left": "2", "right": "5-2", "value": 5},
+      {"id": "5-2", "left": null, "right": null, "value": 5},
+      {"id": "2", "left": "1", "right": null, "value": 2},
+      {"id": "1", "left": null, "right": null, "value": 1}
+    ],
+    "root": "10"
+  }
+}
+
