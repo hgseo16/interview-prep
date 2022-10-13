09ab967ea2f73672978a5a30fbeab7664395c4e9
hgseo16 <seohyounggyoo@gmail.com>
2022-10-13 22:57:29 +0900

    dfsIterative, dfsRecursive, bfsIterative

--- graphs/dfs_bfs.py
+++ graphs/dfs_bfs.py
@@ -1,16 +1,5 @@
-def depthFirstPrintIterative(graph, source):
-    stack = [source]
-    while len(stack) > 0:
-        current = stack.pop()
-        print(current)
-
-        for edges in graph[current]:
-            stack.append(edges)
-
-# def depthFirstPrintRecursive(graph, source):
-
 graph = {
-	"a": ['c', 'b'],
+	"a": ['b', 'c'],
 	"b": ['d'],
 	"c": ['e'],
 	"d": ['f'],
@@ -18,4 +7,38 @@ graph = {
 	"f": []
 }
 
+def depthFirstPrintIterative(graph, source):
+    stack = [source]
+    while len(stack) > 0:
+        current = stack.pop()
+        print(current)
+
+        for neighbor in graph[current]:
+            stack.append(neighbor)
+
+print('DFS Iterative')
 depthFirstPrintIterative(graph, 'a')
+print('-------------------------')
+
+def depthFirstPrintRecursive(graph, source):
+    print(source)
+    for neighbor in graph[source]:
+        depthFirstPrintRecursive(graph, neighbor)
+
+print('DFS Recursive')
+depthFirstPrintRecursive(graph, 'a')
+print('-------------------------')
+
+def breadthFirstPrintIterative(graph, source):
+    queue = [source]
+    while len(queue) > 0:
+        # pop(0) is a dequeue
+        current = queue.pop(0)
+        print(current)
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+
+print('BFS Iterative')
+breadthFirstPrintIterative(graph, 'a')
+print('-------------------------')
+
