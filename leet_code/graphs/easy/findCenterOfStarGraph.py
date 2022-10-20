d01a6a83871910d74718690895950921f0251736
hgseo16 <seohyounggyoo@gmail.com>
2022-10-20 22:56:33 +0900

    findCenterOfStarGraph.py solution

--- leet_code/graphs/easy/findCenterOfStarGraph.py
+++ leet_code/graphs/easy/findCenterOfStarGraph.py
@@ -1,3 +1,14 @@
+from collections import defaultdict
+
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
         
+        graph = defaultdict(list)
+        
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+        for key in graph:
+            if len(graph[key]) > 1:
+                return key
