b4f9a57d5b631fd839bc142f8bc00d4d124046af
hgseo16 <seohyounggyoo@gmail.com>
2022-10-20 23:32:06 +0900

    keysAndRooms.py

--- leet_code/graphs/medium/keysAndRooms.py
+++ leet_code/graphs/medium/keysAndRooms.py
@@ -1,3 +1,26 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         
+        graph = defaultdict(list)
+        
+        for i, room in enumerate(rooms):
+            graph[i] = []
+            for key in room:
+                graph[i].append(key)
+                
+        stack = [0]
+        visited = [False]*len(rooms)
+        
+        while stack:
+            curr = stack.pop()
+            visited[curr] = True
+            for neighbor in graph[curr]:
+                if visited[neighbor] == False:
+                    stack.append(neighbor)
+                    visited[neighbor] = True
+            if graph[curr] == []:
+                visited[curr] = True
+                
+        if False in visited:
+            return False
+        return True
